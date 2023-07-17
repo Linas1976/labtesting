468bc447ebf413e466b7ec01c8886d9afe79fec3
Linas1976 <linas.orantas@gmail.com>
2023-07-17 21:00:17 +0000

    g
    Signed-off-by: Linas1976 <linas.orantas@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+
+InterruptIn bttn(PC_13);
+
+void bttn_interrupt(){
+    printf("bttn pressed \r\n");
+}
+
 int main()
 {
+    bttn.fall(&bttn_interrupt);
     while (true) {
-
     }
 }
 
